896e8eb0c2c5b5bf67f0cc1ff0652cd1892ff3a7
Ilia Ilmer <contact@iliailmer.com>
2025-12-25 19:41:11 -0500

    refactor solvers

--- tinyflow/solver/euler.py
+++ tinyflow/solver/euler.py
@@ -1,13 +1,24 @@
 from collections.abc import Callable
 
+from tinyflow.nn import BaseNeuralNetwork
 from tinyflow.solver.solver import ODESolver
 
 
+def identity(t, rhs_prev):
+    return t
+
+
 class Euler(ODESolver):
-    def __init__(self, rhs_fn: Callable):
+    def __init__(
+        self,
+        rhs_fn: Callable | BaseNeuralNetwork,
+        preprocess_hook: Callable = identity,
+    ):
         super().__init__(rhs_fn)
+        self.preprocess_hook = preprocess_hook
 
     def step(self, h, t, rhs_prev):
+        t = self.preprocess_hook(t, rhs_prev)
         return rhs_prev + h * self.rhs(rhs_prev, t)
 
     def sample(self, h, t, rhs_prev):
--- tinyflow/solver/midpoint.py
+++ tinyflow/solver/midpoint.py
@@ -1,16 +1,26 @@
 from collections.abc import Callable
 
+from tinyflow.nn import BaseNeuralNetwork
 from tinyflow.solver.solver import ODESolver
 
 
+def identity(t, rhs_prev):
+    return t
+
+
 class MidpointSolver(ODESolver):
-    def __init__(self, rhs_fn: Callable):
+    def __init__(
+        self,
+        rhs_fn: Callable | BaseNeuralNetwork,
+        preprocess_hook: Callable = identity,
+    ):
         super().__init__(rhs_fn)
+        self.preprocess_hook = preprocess_hook
 
     def step(self, h, t, rhs_prev):
-        return rhs_prev + h * self.rhs(rhs_prev + h / 2 * self.rhs(rhs_prev, t), t + h / 2)
+        t1 = self.preprocess_hook(t, rhs_prev)
+        t2 = self.preprocess_hook(t + h / 2, rhs_prev + h / 2 * self.rhs(rhs_prev, t1))
+        return rhs_prev + h * self.rhs(rhs_prev + h / 2 * self.rhs(rhs_prev, t1), t2)
 
     def sample(self, h, t, rhs_prev):
-        t = t.reshape((1, 1))
-        t = t.repeat(rhs_prev.shape[0], 1)
         return self.step(h, t, rhs_prev)
